dab239af21dcbb31cd50f91120f641ad76179589
Kimberly Aviles <01010110.v@gmail.com>
2018-10-31 10:09:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(s)
   if s == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif s.upcase?
+    return "NO, NOT SINCE 1938!"    
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
