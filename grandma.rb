abd6ee261cd4226423ef282c0dc773fd0757285b
Kimberly Aviles <01010110.v@gmail.com>
2018-10-31 09:45:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,13 +12,5 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(s)
-  s = gets.strip
-  if .downcase?(s) do
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif s = "I LOVE YOU GRANDMA!
-      return "I LOVE YOU TOO PUMPKIN!"
-  else .upcase?(s)
-        return "NO, NOT SINCE 1938!"
-  end
+def speak_to_grandma()
 end
